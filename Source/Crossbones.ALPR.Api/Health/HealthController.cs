4e2aa5f8d79308f37a22a18cba5da3947f1924ea
muhammad.affan <muhammad.affan@hyperdatacomputing.com>
2023-07-26 10:58:20 +0000

    Merged in GEP-7669_Add/VerifyHealthEndpoints (pull request #26)
    GEP-7669 Add/Verify Health Endpoints
    
    * HealthController added for Consul, Consul Configuration updated. "HealthCheck" endpoint added in each controller except HealthController.
    
    
    Approved-by: Mehmood Usman

---
+++ Source/Crossbones.ALPR.Api/Health/HealthController.cs
@@ -0,0 +1,18 @@
+﻿using Crossbones.Modules.Api;
+using Crossbones.Modules.Api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Crossbones.ALPR.Api.Health
+{
+    [Route("[controller]")]
+    public class HealthController : ApiController
+    {
+        public HealthController(ApiParams feature) : base(feature) { }
+
+        [HttpGet]
+        public async Task<IActionResult> Status()
+        {
+            return Ok("ALPR service is running");
+        }
+    }
+}
